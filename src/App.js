1ece95a83feea4709162278a0b1745665b875f4e
Rutik <rutikkulkarni2001@gmail.com>
2024-09-25 21:42:44 +0530

    init React setup with TailwindCSS

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto p-4">
+        <h1 className="text-4xl text-center font-bold mb-4">Expense Tracker</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
